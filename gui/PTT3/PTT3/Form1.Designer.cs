4dd372ed5ad94eb6f2d36339243267c1cd5c3907
Bart Kneepkens <bkneepkens@msn.com>
2016-11-09 15:19:15 +0100

    De cells in de DataGridView veranderen al van kleur, maar niet meteen nadat er geklikt word. op een of andere manier wordt het niet ge-update.

--- gui/PTT3/PTT3/Form1.Designer.cs
+++ gui/PTT3/PTT3/Form1.Designer.cs
@@ -89,6 +89,7 @@
             this.dataGridView1.Size = new System.Drawing.Size(250, 250);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellFormatting);
             // 
             // Column1
             // 
--- gui/PTT3/PTT3/Form1.cs
+++ gui/PTT3/PTT3/Form1.cs
@@ -12,20 +12,17 @@ namespace PTT3
 {
     public partial class Form1 : Form
     {
+        int[,] maze;
         public Form1()
         {
             InitializeComponent();
             dataGridView1.RowCount = 10;
+            maze = new int[10,10];
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             MessageBox.Show("Test");
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
@@ -36,7 +33,18 @@ namespace PTT3
             row = dataGridView1.CurrentCell.RowIndex;
             column = dataGridView1.CurrentCell.ColumnIndex;
 
-            MessageBox.Show("Row and column: " + row + "," + column);
+            System.Diagnostics.Debug.WriteLine((column + "," + row + " selected."));
+
+            maze[row,column] = 1;
+            dataGridView1.Refresh();
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+                    if (maze[e.RowIndex,e.ColumnIndex] == 1)
+                    {
+                        e.CellStyle.BackColor = Color.Black;
+                    }
         }
     }
 }
