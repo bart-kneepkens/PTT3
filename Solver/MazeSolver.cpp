1e34128179c712cc88a816479961aa7323008cc9
Martin Sebastiaan Donkersloot <martin.donkersloot@outlook.com>
2016-11-23 12:24:23 +0100

    Fixed headers and source files, everything builds now.

--- Solver/MazeSolver.cpp
+++
@@ -1,6 +0,0 @@
-#include "../../Documents/VPProjects/Solver.h"
-
-void Solver::StartSolving(int grid[][]) {
-	// TODO - implement Solver::StartSolving
-	throw "Not yet implemented";
-}
--- Solver/MazeSolver.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef SOLVER_H
-#define SOLVER_H
-private:
-	
-#endif
