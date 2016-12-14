b752e7219fa0ef848fe13e5960cfda16b77bf990
Bart Kneepkens <bkneepkens@msn.com>
2016-12-14 11:12:23 +0100

    It's a start!

---
+++ Server/Server.cpp
@@ -0,0 +1,20 @@
+
+int main(){
+    // Wait for modules to send connect message async
+    
+    // Wait for user to give module order async
+    
+    // Wait for user to give Start signal async
+}
+
+int StartSequence(){
+    
+}
+
+int setOrder(){
+    
+}
+
+int connectModule(){
+    
+}
