69aa379cb5df03a9a75bd90aa0b411e188c5857d
Yamil <yam2005@live.com>
2016-12-21 14:43:33 +0100

    Initial program main for local testing

--- Plotter/PlotterProgram.cpp
+++ Plotter/PlotterProgram.cpp
@@ -1,9 +1,44 @@
 #include "PlotterController.h"
-#include "MazeParser.hpp"
+#include "../maze_parser/MazeParser.hpp"
+#include <iostream>
 
-#include "IModule.hpp"
+#include "../Module/IModule.hpp"
+
+std::vector<std::vector<char>> testScan;
+
+char array[10][10] =
+	{
+		{' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
+		{' ','#',' ','#','#','#','#','#','#',' '},
+		{' ','#',' ','#',' ',' ',' ',' ','#',' '},
+		{' ','#',' ','#','#','#',' ','#','#',' '},
+		{' ','#',' ',' ',' ',' ',' ','#','#',' '},
+		{' ','#','#','#','#',' ','#','#','#',' '},
+		{' ','#',' ',' ','#',' ','#',' ','#',' '},
+		{' ','#',' ','#','#',' ','#',' ','#',' '},
+		{' ','#',' ',' ',' ',' ',' ',' ','#',' '},
+		{' ','#','#',' ','#','#','#','#','#',' '}	
+	};
+
+std::vector<std::vector<char>> ArrayToVector(char in[10][10])
+{
+	std::vector<std::vector<char>> temp;
+	for(int i = 0; i < 10; i++)
+	{
+		for(int j = 0; j < 10; j++)
+		{
+			temp[i][j] = in[i][j];
+
+			std::cout << in[i][j];
+		}
+
+		std::cout << std::endl;
+	}
+
+	return temp;
+}
 
 int main()
 {
-	
+	testScan = ArrayToVector(array);
 }
