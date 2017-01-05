6cc7c29ddef7c00acf36cb41bed7ee3241044014
Bart Kneepkens <bkneepkens@msn.com>
2017-01-05 20:14:28 +0100

    Maybe i'll revert this.

--- Logger/Logger.cpp
+++ Logger/Logger.cpp
@@ -9,70 +9,70 @@
 #include <string.h>
 
 class Logger {
-	public:
-        static Logger& getInstance(){
-            static Logger instance; // Guaranteed to be destroyed.      
-            return instance; // Instantiated on first use.
+public:
+    static Logger& getInstance(){
+        static Logger instance; // Guaranteed to be destroyed.
+        return instance; // Instantiated on first use.
+    }
+    
+    void logMessage(std::string message){
+        LogMessage msg;
+        
+        strcpy(msg.text, message.c_str());
+        time(&msg.timestamp);
+        
+        sem_wait(&(buff->empty));
+        
+        addToBuffer(msg);
+        
+        sem_post(&(buff->filled));
+        
+        std::cout << "Logged Message: " << msg.text << ":" << msg.timestamp << std::endl;
+    }
+private:
+    char* SHM_NAME;
+    
+    struct LogBuffer* buff;
+    
+    int shm_fd;
+    
+    Logger() {
+        SHM_NAME = "LogBuffer";
+        
+        // Get shared memory file descriptor.
+        if ((shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666)) == -1){
+            perror("cannot open");
         }
         
-        void logMessage(std::string message){
-			LogMessage msg;
-			
-			strcpy(msg.text, message.c_str());
-			time(&msg.timestamp);
-			
-			sem_wait(&(buff->empty));
-			
-			addToBuffer(msg);
-			
-			sem_post(&(buff->filled));
-			
-			std::cout << "Logged Message: " << msg.text << ":" << msg.timestamp << std::endl;
-		}
-    private:    
-        char* SHM_NAME;
-		
-		struct LogBuffer* buff;
-
-		int shm_fd;
-		
-        Logger() {
-			SHM_NAME = "LogBuffer";
-			
-			// Get shared memory file descriptor.
-			if ((shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666)) == -1){
-				perror("cannot open");
-			}
+        // Set the shared memory size to the size of LogBuffer struct.
+        if (ftruncate(shm_fd, sizeof(LogBuffer)) != 0){
+            perror("cannot set size");
+        }
+        
+        // Map shared memory in address space.
+        if ((buff = (struct LogBuffer *) mmap(0, sizeof(LogBuffer), PROT_WRITE, MAP_SHARED, shm_fd, 0)) == MAP_FAILED){
+            perror("cannot mmap");
+        }
+        
+        // Lock the shared memory.
+        if (mlock(buff, sizeof(LogBuffer)) != 0){
+            perror("cannot mlock");
+        }
+        
+        // Shared memory is ready for use.
+        printf("LOGGER: Shared Memory successfully opened.\n");
+        
+    }
     
-			// Set the shared memory size to the size of LogBuffer struct.
-			if (ftruncate(shm_fd, sizeof(LogBuffer)) != 0){
-				perror("cannot set size");
-			}
+    Logger(Logger const&);              // Don't Implement
+    void operator=(Logger const&); 		// Don't implement
     
-			// Map shared memory in address space.
-			if ((buff = (struct LogBuffer *) mmap(0, sizeof(LogBuffer), PROT_WRITE, MAP_SHARED, shm_fd, 0)) == MAP_FAILED){
-				perror("cannot mmap");
-			}
-			
-			// Lock the shared memory.
-			if (mlock(buff, sizeof(LogBuffer)) != 0){
-				perror("cannot mlock");
-			}
-    
-			// Shared memory is ready for use.
-			printf("LOGGER: Shared Memory successfully opened.\n");
-	
-		}
-		                    
-        Logger(Logger const&);              // Don't Implement
-        void operator=(Logger const&); 		// Don't implement
-
-		void addToBuffer(LogMessage msg){
-			for (int i = 0; i < 9; i++){
-				buff->messages[i] = buff->messages[i+1];
-			}
-			buff->messages[9] = msg;
-		}
+    void addToBuffer(LogMessage msg){
+        for (int i = 0; i < 9; i++){
+            buff->messages[i] = buff->messages[i+1];
+        }
+        buff->messages[9] = msg;
+    }
 };
 
 
--- Logger/LoggerCentral.cpp
+++ Logger/LoggerCentral.cpp
@@ -7,80 +7,88 @@
 #include <fcntl.h>
 #include <fstream>
 
-
-const char* SHM_NAME = "LogBuffer";
-
-struct LogBuffer* buff;
-
-int shm_fd;
-
-LogMessage takeLastFromBuffer(){
-	LogMessage last = buff->messages[9];
-
-	for(int i = 1; i < 9; i++){
-		buff->messages[i -1 ] = buff->messages[i];
-	}
-	
-	return last;
-}
-
-int main(){
-	munmap(buff, sizeof(LogBuffer));
-	
-	// Get shared memory file descriptor.
-    if ((shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666)) == -1){
-        perror("cannot open");
-        return -1;
+class LoggerCentral {
+public:
+    LoggerCentral(){
+        SHM_NAME = "LogBuffer";
+        
+        //munmap(buff, sizeof(LogBuffer));
+        
+        // Get shared memory file descriptor.
+        if ((shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666)) == -1){
+            perror("cannot open");
+            //return -1;
+        }
+        
+        // Set the shared memory size to the size of LogBuffer struct.
+        if (ftruncate(shm_fd, sizeof(LogBuffer)) != 0){
+            perror("cannot set size");
+            //return -1;
+        }
+        
+        // Map shared memory in address space.
+        if ((buff = (struct LogBuffer *) mmap(0, sizeof(LogBuffer), PROT_WRITE, MAP_SHARED, shm_fd, 0)) == MAP_FAILED){
+            perror("cannot mmap");
+            //return -1;
+        }
+        
+        // Lock the shared memory.
+        if (mlock(buff, sizeof(LogBuffer)) != 0){
+            perror("cannot mlock");
+            //return -1;
+        }
+        
+        // Shared memory is ready for use.
+        printf("Shared Memory successfully opened.\n");
+        
+        // Initialize semaphore 'filled' with value 0.
+        if(sem_init(&(buff->filled), 1, 0) != 0){
+            perror("Can not init semaphore 'filled'");
+            //return -1;
+        }
+        
+        // Initialize semaphore 'empty' with value 10.
+        if(sem_init(&(buff->empty), 1, 10) != 0){
+            perror("Can not init semaphore 'empty'");
+            //return -1;
+        }
     }
     
-    // Set the shared memory size to the size of LogBuffer struct.
-    if (ftruncate(shm_fd, sizeof(LogBuffer)) != 0){
-        perror("cannot set size");
-        return -1;
+    void run() {
+        // Open LogFile
+        std::ofstream logFile;
+        logFile.open("MazeSolverLogs", std::ofstream::out | std::ofstream::app);
+        char dateFormatted[18];
+        struct tm *tm;
+        
+        // Wait for Logger class to make isRady true before entering
+        // Else the semaphores are not initialised resulting in a deadlock.
+        while(true){
+            std::cout << "waiting" << std::endl;
+            sem_wait(&(buff->filled));
+            LogMessage msg = takeLastFromBuffer();
+            sem_post(&(buff->empty));
+            tm = localtime(&(msg.timestamp));
+            strftime(dateFormatted, sizeof(dateFormatted), "%D,%T", tm);
+            logFile << dateFormatted << " :: " << msg.text << std::endl;
+            std::cout << dateFormatted << " :: " << msg.text << std::endl;
+        }
     }
     
-    // Map shared memory in address space.
-    if ((buff = (struct LogBuffer *) mmap(0, sizeof(LogBuffer), PROT_WRITE, MAP_SHARED, shm_fd, 0)) == MAP_FAILED){
-        perror("cannot mmap");
-        return -1;
+private:
+    char* SHM_NAME;
+    
+    struct LogBuffer* buff;
+    
+    int shm_fd;
+    
+    LogMessage takeLastFromBuffer(){
+        LogMessage last = buff->messages[9];
+        
+        for(int i = 1; i < 9; i++){
+            buff->messages[i -1 ] = buff->messages[i];
+        }
+        
+        return last;
     }
-    
-    // Lock the shared memory.
-    if (mlock(buff, sizeof(LogBuffer)) != 0){
-        perror("cannot mlock");
-        return -1;
-    }
-    
-    // Shared memory is ready for use.
-    printf("Shared Memory successfully opened.\n");
-    
-    // Initialize semaphore 'filled' with value 0.
-	if(sem_init(&(buff->filled), 1, 0) != 0){
-		perror("Can not init semaphore 'filled'");
-		return -1;
-	}
-
-	// Initialize semaphore 'empty' with value 10.
-	if(sem_init(&(buff->empty), 1, 10) != 0){
-		perror("Can not init semaphore 'empty'");
-		return -1;
-	}
-	
-	// Open LogFile
-	std::ofstream logFile;
-	logFile.open("MazeSolverLogs", std::ofstream::out | std::ofstream::app);
-	char dateFormatted[18];
-	struct tm *tm;
-  
-    // Wait for Logger class to make isRady true before entering
-    // Else the semaphores are not initialised resulting in a deadlock.
-    while(true){
-		sem_wait(&(buff->filled));
-		LogMessage msg = takeLastFromBuffer();
-		sem_post(&(buff->empty));
-		tm = localtime(&(msg.timestamp));
-		strftime(dateFormatted, sizeof(dateFormatted), "%D,%T", tm);
-		logFile << dateFormatted << " :: " << msg.text << std::endl;
-		std::cout << dateFormatted << " :: " << msg.text << std::endl;
-	}
-}
+};
