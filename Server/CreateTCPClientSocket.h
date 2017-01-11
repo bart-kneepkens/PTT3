2661b9966d261a7699a773aeb7b4e2ec717b01d1
Martin Sebastiaan Donkersloot <martin.donkersloot@outlook.com>
2017-01-11 14:06:15 +0100

    Removed outdated Server project.

--- Server/CreateTCPClientSocket.h
+++
@@ -1,6 +0,0 @@
-#ifndef _CREATE_TCP_CLIENT_SOCKET_H_
-#define _CREATE_TCP_CLIENT_SOCKET_H_
-
-extern int CreateTCPClientSocket (const char * addr, unsigned short port); /* Create TCP server socket */
-
-#endif
