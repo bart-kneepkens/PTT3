e7e689e9dd5f6be39133ccfec47266e57c46353e
Bart Kneepkens <bkneepkens@msn.com>
2017-01-11 15:08:26 +0100

    Delete unused folder.

--- Common/LogBuffer.h
+++
@@ -1,8 +0,0 @@
-#include "LogMessage.h"
-#include <semaphore.h>
-
-struct LogBuffer {
-	LogMessage messages[10];
-	sem_t filled;
-	sem_t empty;
-};
--- Common/LogMessage.h
+++
@@ -1,6 +0,0 @@
-#include <time.h>
-
-struct LogMessage {
-	char text[256];
-	time_t timestamp;
-};
