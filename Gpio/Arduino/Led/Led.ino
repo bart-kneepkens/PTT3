3f2a31884643372c3ce7f8cf372ccdbedc4ca4d6
Bart Kneepkens <bkneepkens@msn.com>
2017-01-11 15:00:49 +0100

    Streamlining and comments.

--- Gpio/Arduino/Led/Led.ino
+++ Gpio/Arduino/Led/Led.ino
@@ -1,12 +1,12 @@
 void setup() {
-  DDRB |= (1<<DDB5);        // Set led pin 13 as an output.
-  DDRD |= ~(1 << DDB2);     // Set pin 2 as an input.
+  DDRB |= (1 << DDB5);        // Set Pin 13 as an output.
+  DDRD |= ~(1 << DDB2);       // Set Pin 2 as an input.
 }
 
 void loop() {
-  if(((PIND >> 2) & 1) == 1){
-    PORTB |= (1 << PORTB5);
+  if(((PIND >> 2) & 1) == 1){ // If Pin 2 reads HIGH
+    PORTB |= (1 << PORTB5);   // Write HIGH to Pin 13
   } else {
-    PORTB &= ~(1 << PORTB5);
+    PORTB &= ~(1 << PORTB5);  // Write LOW to Pin 13
   }
 }
